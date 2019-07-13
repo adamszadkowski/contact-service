dc08875e1d888a471e5a573b7663b48d4afac3ab
Adam Szadkowski <adam@szadkowski.info>
2019-07-13 15:11:12 +0200

    Add integrationTest gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,23 @@ plugins {
   id("io.franzbecker.gradle-lombok") version "3.1.0"
 }
 
+sourceSets {
+  create("intTest") {
+    compileClasspath += sourceSets.main.get().output
+    runtimeClasspath += sourceSets.main.get().output
+  }
+}
+
+val intTestImplementation by configurations.getting {
+  extendsFrom(configurations.implementation.get())
+  extendsFrom(configurations.testImplementation.get())
+}
+
+val intTestRuntimeOnly by configurations.getting {
+  extendsFrom(configurations.runtimeOnly.get())
+  extendsFrom(configurations.testRuntimeOnly.get())
+}
+
 tasks.getByName<BootJar>("bootJar") {
   archiveFileName.set("contact-service.jar")
 }
@@ -45,3 +62,16 @@ dependencies {
 tasks.test {
   useJUnitPlatform()
 }
+
+val integrationTest = task<Test>("integrationTest") {
+  description = "Runs integration tests."
+  group = "verification"
+
+  testClassesDirs = sourceSets["intTest"].output.classesDirs
+  classpath = sourceSets["intTest"].runtimeClasspath
+
+  useJUnitPlatform()
+  shouldRunAfter("test")
+}
+
+tasks.check { dependsOn(integrationTest) }
