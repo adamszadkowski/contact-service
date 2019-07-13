f1aefdcc32b60d994bd7e598bee2ba2997327a58
Adam Szadkowski <adam@szadkowski.info>
2019-07-13 18:30:39 +0200

    Remove unnecessary environment configuration

--- src/test/java/info/szadkowski/contact/properties/ApplicationEnvironmentConfigurationTest.java
+++ src/test/java/info/szadkowski/contact/properties/ApplicationEnvironmentConfigurationTest.java
@@ -14,7 +14,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
@@ -75,60 +74,6 @@ class ApplicationEnvironmentConfigurationTest {
     @Test
     void shouldSetupServerPort(@Value("${server.port}") int serverPort) {
       assertThat(serverPort).isEqualTo(80);
-    }
-  }
-
-  @Nested
-  @ContextConfiguration(classes = {
-          MailAddressesProperties.class,
-          MailProperties.class,
-          ThrottlingProperties.class,
-          YamlConfiguration.class
-  })
-  @TestPropertySource(
-          properties = {
-                  "MAIL_RECIPIENT_MAIL=recipient@address.com",
-                  "MAIL_SENDER_MAIL=sender@address.com",
-                  "MAIL_SERVER_DOMAIN=domain.com",
-                  "MAIL_SERVER_PORT=123",
-                  "MAIL_SERVER_USERNAME=username",
-                  "MAIL_SERVER_PASSWORD=password",
-                  "THROTTLING_CLEAR_EXPIRED_RATE=5h",
-                  "THROTTLING_IP_LIMIT=1",
-                  "THROTTLING_IP_WINDOW=2h",
-                  "THROTTLING_ALL_LIMIT=3",
-                  "THROTTLING_ALL_WINDOW=4h"
-          })
-  @EnableConfigurationProperties
-  class ConfiguredByUser {
-
-    @Test
-    void shouldInjectAddresses(@Autowired MailAddressesProperties properties) {
-      assertThat(properties.getRecipientMail()).isEqualTo("recipient@address.com");
-      assertThat(properties.getSenderMail()).isEqualTo("sender@address.com");
-    }
-
-    @Test
-    void shouldInjectThrottling(@Autowired ThrottlingProperties properties) {
-      assertThat(properties.getClearExpiredRate()).isEqualTo(Duration.ofHours(5));
-      assertThat(properties.getIp().getLimit()).isEqualTo(1);
-      assertThat(properties.getIp().getWindow()).isEqualTo(Duration.ofHours(2));
-      assertThat(properties.getAll().getLimit()).isEqualTo(3);
-      assertThat(properties.getAll().getWindow()).isEqualTo(Duration.ofHours(4));
-    }
-
-    @Test
-    void shouldConfigureMailStarter(@Autowired MailProperties properties) {
-      assertThat(properties.getHost()).isEqualTo("domain.com");
-      assertThat(properties.getPort()).isEqualTo(123);
-      assertThat(properties.getUsername()).isEqualTo("username");
-      assertThat(properties.getPassword()).isEqualTo("password");
-      assertThat(properties.getProperties())
-              .hasSize(4)
-              .containsEntry("mail.smtp.auth", "true")
-              .containsEntry("mail.smtp.starttls.enable", "true")
-              .containsEntry("mail.smtp.starttls.required", "true")
-              .containsEntry("mail.smtp.ssl.trust", "domain.com");
     }
   }
 
