52b65c25088b023857df2117ba5b0eb12e4aa165
Adam Szadkowski <adam@szadkowski.info>
2019-07-13 16:08:32 +0200

    Move throttling tests to integration suite

--- src/test/java/info/szadkowski/contact/controller/MessageControllerTest.java
+++ src/test/java/info/szadkowski/contact/controller/MessageControllerTest.java
@@ -1,51 +1,43 @@
 package info.szadkowski.contact.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import info.szadkowski.contact.controller.exception.ExceptionHandlerController;
 import info.szadkowski.contact.model.MessageRequest;
-import info.szadkowski.contact.properties.MailAddressesProperties;
 import info.szadkowski.contact.throttle.Throttler;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Value;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(MockitoExtension.class)
 class MessageControllerTest {
-  private static final ObjectMapper MAPPER = new ObjectMapper();
+  private List<MessageRequest> messages;
 
-  private List<MessageRequest> mails;
-  private MailAddressesProperties mailAddressesProperties;
-  private MockedThrottler ipThrottler;
-  private MockedThrottler allThrottler;
+  @Mock
+  private Throttler throttler;
+
   private MockMvc mvc;
 
   @BeforeEach
   void setUp() {
-    mails = new ArrayList<>();
-    mailAddressesProperties = new MailAddressesProperties();
-    ipThrottler = new MockedThrottler();
-    allThrottler = new MockedThrottler();
+    messages = new ArrayList<>();
+    Mockito.when(throttler.canProcess(Mockito.anyString())).thenReturn(true);
     MessageController messageController = new MessageController(
-            mails::add,
+            messages::add,
             r -> "templated:" + r.get("content"),
-            ipThrottler,
-            allThrottler);
+            throttler,
+            throttler);
     mvc = MockMvcBuilders.standaloneSetup(messageController)
             .setControllerAdvice(new ExceptionHandlerController())
             .build();
@@ -53,129 +45,18 @@ class MessageControllerTest {
 
   @Test
   void shouldSendCorrectMailRequest() throws Exception {
-    mailAddressesProperties.setRecipientMail("recipient@address.com");
-    mailAddressesProperties.setSenderMail("sender@address.com");
-    mvc.perform(
-            post("/v1/message")
-                    .contentType(MediaType.APPLICATION_JSON_UTF8)
-                    .content(serialize(SampleMessage.builder()
-                            .subject("mySubject")
-                            .content("myContent")
-                            .build()))
+    mvc.perform(post("/v1/message")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content("" +
+                    "{" +
+                    "\"subject\": \"mySubject\"," +
+                    "\"content\": \"myContent\"" +
+                    "}")
     ).andExpect(status().isOk());
 
-    assertThat(mails).containsExactly(MessageRequest.builder()
+    assertThat(messages).containsExactly(MessageRequest.builder()
             .subject("mySubject")
             .content("templated:myContent")
             .build());
-  }
-
-  @Nested
-  class Throttling {
-
-    @Test
-    void shouldNotSendWhenThrottlingLimitHasBeenReached() throws Exception {
-      ipThrottler.setCannotProcess("10.0.0.1");
-
-      mvc.perform(
-              post("/v1/message")
-                      .with(remoteAddr("10.0.0.1"))
-                      .contentType(MediaType.APPLICATION_JSON_UTF8)
-                      .content(serialize(SampleMessage.builder()
-                              .subject("mySubject")
-                              .content("myContent")
-                              .build()))
-      ).andExpect(status().isTooManyRequests());
-      assertThat(mails).isEmpty();
-    }
-
-    @Test
-    void shouldSendWhenLimitHasBeenReachedForOtherIp() throws Exception {
-      ipThrottler.setCannotProcess("10.0.0.1");
-
-      mvc.perform(
-              post("/v1/message")
-                      .with(remoteAddr("10.0.0.2"))
-                      .contentType(MediaType.APPLICATION_JSON_UTF8)
-                      .content(serialize(SampleMessage.builder()
-                              .subject("mySubject")
-                              .content("myContent")
-                              .build()))
-      ).andExpect(status().isOk());
-      assertThat(mails).hasSize(1);
-    }
-
-    @Test
-    void shouldThrottleIndependentFromIp() throws Exception {
-      allThrottler.setCannotProcess("all");
-
-      mvc.perform(
-              post("/v1/message")
-                      .with(remoteAddr("10.0.0.1"))
-                      .contentType(MediaType.APPLICATION_JSON_UTF8)
-                      .content(serialize(SampleMessage.builder()
-                              .subject("mySubject")
-                              .content("myContent")
-                              .build()))
-      ).andExpect(status().isTooManyRequests());
-      assertThat(mails).isEmpty();
-    }
-
-    @Test
-    void shouldCheckIpThrottlerFirstAndFailFast() throws Exception {
-      ipThrottler.setCannotProcess("10.0.0.1");
-
-      mvc.perform(
-              post("/v1/message")
-                      .with(remoteAddr("10.0.0.1"))
-                      .contentType(MediaType.APPLICATION_JSON_UTF8)
-                      .content(serialize(SampleMessage.builder()
-                              .subject("mySubject")
-                              .content("myContent")
-                              .build()))
-      ).andExpect(status().isTooManyRequests());
-
-      assertThat(ipThrottler.getCanProcessCounter()).isEqualTo(1);
-      assertThat(allThrottler.getCanProcessCounter()).isEqualTo(0);
-    }
-
-    private RequestPostProcessor remoteAddr(String remoteAddr) {
-      return request -> {
-        request.setRemoteAddr(remoteAddr);
-        return request;
-      };
-    }
-  }
-
-  private static String serialize(Object v) throws JsonProcessingException {
-    return MAPPER.writeValueAsString(v);
-  }
-
-  @Value
-  @Builder
-  private static class SampleMessage {
-    private String subject;
-    private String content;
-  }
-
-  private static class MockedThrottler implements Throttler {
-    private Map<String, Boolean> allow = new HashMap<>();
-
-    @Getter
-    private int canProcessCounter = 0;
-
-    @Override
-    public boolean canProcess(String key) {
-      canProcessCounter++;
-      return allow.getOrDefault(key, true);
-    }
-
-    private void setCannotProcess(String key) {
-      allow.put(key, false);
-    }
-
-    @Override
-    public void clearExpired() {
-    }
   }
 }
