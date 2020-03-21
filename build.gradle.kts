ddd65adcd8674a165e479a53d07da0a902579c6a
Adam Szadkowski <adam@szadkowski.info>
2020-03-21 11:36:26 +0100

    Change JVM target to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
     getByName<BootJar>("bootJar") {
         archiveFileName.set("contact-service.jar")
